279d3d3fbe485d49c84b7d3c6309bcd2c35cf1de
Rayelis <116190430+Rayelis@users.noreply.github.com>
2023-07-17 21:20:29 +0400

    Add files via upload

---
+++ site1-sub_category/site1-sub_category/script.js
@@ -0,0 +1,80 @@
+function change_text(Element){
+
+    let buttons = document.querySelectorAll(".main-nav-button")
+    let texts = document.querySelectorAll(".main-content-text")
+
+    let button_index
+
+    let clicked_button
+    let chosen_text
+
+    buttons.forEach( button => {
+        button.classList.add("nav-passive")
+        button.classList.remove("nav-active")
+    })
+
+    texts.forEach( text => {
+        text.classList.add("hidden")
+    })
+
+    for(let i = 0; i < buttons.length; i++){
+        if(buttons[i] === Element){
+            button_index = i
+        }
+    }
+
+    clicked_button = buttons[button_index]
+    chosen_text = texts[button_index]
+
+    clicked_button.classList.add("nav-active")
+    clicked_button.classList.remove("nav-passive")
+    chosen_text.classList.remove("hidden")
+}
+
+function show_black_screen(){
+
+    let black_screen = document.querySelector(".black-screen")
+    black_screen.classList.remove("hidden")
+
+}
+
+function hide_black_screen(){
+
+    let black_screen = document.querySelector(".black-screen")
+    black_screen.classList.add("hidden")
+
+}
+
+function show_feedback_form(){
+
+    let form = document.querySelector(".feedback-form")
+    show_black_screen()
+    form.classList.remove("hidden")
+
+}
+
+function hide_feedback_form(){
+
+    let form = document.querySelector(".feedback-form")
+    hide_black_screen()
+    form.classList.add("hidden")
+
+}
+
+
+let is_sidebar_shown = false
+function show_sidebar(){
+    
+    let sidebar = document.querySelector(".mobile-sidebar")
+
+    if(is_sidebar_shown){
+        sidebar.removeAttribute("style")
+        is_sidebar_shown = false
+        return
+    } 
+    
+    sidebar.style.transform = "translateX(0)"
+    is_sidebar_shown = true
+
+}
+
